4b3677fe3899929dc2e50eff6cdf778793ee92b9
Trang Le <trang.le@mail.finviet.com.vn>
2024-06-14 15:33:58 +0700

    fix: layout sign in, signup

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,55 +3,68 @@ import {
   ProFormCheckbox,
   ProFormText,
 } from "@ant-design/pro-components";
+import logInImage from "../../assets/login.svg";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../utils/constants";
+import { Col, Row } from "antd";
+
+const layout = {
+  wrapperCol: { span: 16 },
+};
 
 const Login = () => {
   return (
-    <div className="login flex h-[100vh]">
-      <div className="login-wrap-form w-[50%] flex flex-col justify-center items-center">
-        <div className="flex-col justify-center px-[40px] w-[500px]">
-          <h1 className="login-title mb-[20px]">SIGN IN</h1>
-          <ProForm
-            className="login-form"
-            submitter={{
-              searchConfig: {
-                submitText: "Sign In",
-              },
-              submitButtonProps: {
-                style: {
-                  width: "100%",
-                },
-              },
-              resetButtonProps: {
-                style: {
-                  display: "none",
-                },
-              },
-            }}
-          >
-            <ProFormText
-              name="username"
-              label="Username"
-              placeholder="Username"
-              rules={[{ required: true, message: "Please enter username" }]}
-            />
-            <ProFormText.Password
-              name="password"
-              label="Password"
-              placeholder="Password"
-              rules={[{ required: true, message: "Please enter password" }]}
-            />
-            <ProFormCheckbox>Remember me</ProFormCheckbox>
-          </ProForm>
+    <div className="login-container">
+      <div className="login flex">
+        <div className="login-right w-[50%] h-[100%]">
+          <img src={logInImage} alt="login image" />
         </div>
-      </div>
-      <div className="login-right w-[50%] h-[100%]">
-        <h2 className="login-welcome-title mb-[20px]">Welcome to login</h2>
-        <p className="login-desc mb-[30px]">Don't have an account?</p>
-        <Link className="login-sign-up" to={ROUTES.signUp}>
-          Sign Up
-        </Link>
+        <div className="login-wrap-form w-[50%] flex flex-col justify-center items-center">
+          <div className="flex-col justify-center px-[40px] mb-[10px]">
+            <h1 className="login-title mb-[20px] text-center">SIGN IN</h1>
+            <ProForm
+              className="login-form"
+              submitter={{
+                searchConfig: {
+                  submitText: "Sign In",
+                },
+                submitButtonProps: {
+                  style: {
+                    width: "100%",
+                  },
+                },
+                resetButtonProps: {
+                  style: {
+                    display: "none",
+                  },
+                },
+              }}
+            >
+              <ProFormText
+                name="username"
+                label="Username"
+                placeholder="Username"
+                rules={[{ required: true, message: "Please enter username" }]}
+                width="md"
+              />
+
+              <ProFormText.Password
+                name="password"
+                label="Password"
+                placeholder="Password"
+                rules={[{ required: true, message: "Please enter password" }]}
+                width="md"
+              />
+              <ProFormCheckbox>Remember me</ProFormCheckbox>
+            </ProForm>
+          </div>
+          <div className="login-content-signup flex">
+            <p className="login-desc mr-[5px]">Don't have an account?</p>
+            <Link className="login-sign-up" to={ROUTES.signUp}>
+              <strong>Sign Up!</strong>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -4,10 +4,11 @@ import {
   ProFormSelect,
   ProFormText,
 } from "@ant-design/pro-components";
+import moment from "moment";
 import { Link, redirect } from "react-router-dom";
+import signUpImage from "../../assets/signup.svg";
 import { ROUTES } from "../../utils/constants";
 import { handleCreateUser } from "../../utils/handler";
-import moment from "moment";
 
 const SignUp = () => {
   const [form] = ProForm.useForm();
@@ -46,95 +47,100 @@ const SignUp = () => {
   };
 
   return (
-    <div className="sign-up flex h-[100vh]">
-      <div className="sign-up-wrap-form w-[60%] flex flex-col justify-center items-center py-[40px]">
-        <h1 className="sign-up-title mb-[20px]">Sign Up</h1>
-        <ProForm
-          submitter={{
-            searchConfig: {
-              submitText: "Create Account",
-            },
-            submitButtonProps: {
-              style: {
-                width: "100%",
-              },
-            },
-            resetButtonProps: {
-              style: {
-                display: "none",
-              },
-            },
-          }}
-          onFinish={handleSignUp}
-          form={form}
-        >
-          <ProFormText
-            name="fullName"
-            label="Full Name"
-            placeholder="Full Name"
-            rules={[{ required: true, message: "Please enter full name" }]}
-            width="md"
-          />
-          <ProFormText
-            name="email"
-            label="Email"
-            placeholder="Email"
-            rules={[
-              {
-                type: "email",
-                message: "The input is not valid E-mail!",
-              },
-              { required: true, message: "Please enter email" },
-            ]}
-            width="md"
-          />
-          <ProFormText.Password
-            name="password"
-            label="Password"
-            placeholder="Password"
-            rules={[{ required: true, message: "Please enter password" }]}
-            width="md"
-          />
-          <ProFormText.Password
-            name="confirm_password"
-            label="Confirm password"
-            placeholder="Confirm password"
-            rules={[
-              { required: true, message: "Please enter confirm password" },
-              {
-                validator: validateConfirmPassword,
-              },
-            ]}
-            width="md"
-          />
-          <div className="flex justify-between">
-            <ProFormSelect
-              name="gender"
-              label="Gender"
-              placeholder="Gender"
-              options={[
-                { label: "Male", value: "MALE" },
-                { label: "Female", value: "FEMALE" },
-                { label: "Other", value: "OTHER" },
-              ]}
-              rules={[{ required: true, message: "Please select gender" }]}
-            />
-            <ProFormDatePicker
-              placeholder="Birthday"
-              name="dob"
-              label="Birthday"
-              rules={[{ required: true, message: "Please enter birthday" }]}
-            />
-          </div>
-        </ProForm>
-        <div className="sign-up-login mt-[20px]">
-          <p className="sign-up-login-desc">
-            Already have an account? <Link to={ROUTES.login}>Log In</Link>
-          </p>
+    <div className="sign-up-container">
+      <div className="sign-up flex">
+        <div className="sign-up-right w-[40%] h-[100%] px-[20px]">
+          <img src={signUpImage} alt="sign up image" />
         </div>
-      </div>
-      <div className="sign-up-right w-[40%] h-[100%] px-[20px]">
-        <h2 className="sign-up-welcome-title mb-[20px]">Welcome to RIGHTFIX</h2>
+        <div className="sign-up-wrap-form w-[60%] flex flex-col justify-center items-center py-[40px]">
+          <h1 className="sign-up-title mb-[20px]">SIGN UP</h1>
+          <ProForm
+            submitter={{
+              searchConfig: {
+                submitText: "Create Account",
+              },
+              submitButtonProps: {
+                style: {
+                  width: "100%",
+                },
+              },
+              resetButtonProps: {
+                style: {
+                  display: "none",
+                },
+              },
+            }}
+            onFinish={handleSignUp}
+            form={form}
+          >
+            <ProFormText
+              name="fullName"
+              label="Full Name"
+              placeholder="Full Name"
+              rules={[{ required: true, message: "Please enter full name" }]}
+              width="md"
+            />
+            <ProFormText
+              name="email"
+              label="Email"
+              placeholder="Email"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!",
+                },
+                { required: true, message: "Please enter email" },
+              ]}
+              width="md"
+            />
+            <ProFormText.Password
+              name="password"
+              label="Password"
+              placeholder="Password"
+              rules={[{ required: true, message: "Please enter password" }]}
+              width="md"
+            />
+            <ProFormText.Password
+              name="confirm_password"
+              label="Confirm password"
+              placeholder="Confirm password"
+              rules={[
+                { required: true, message: "Please enter confirm password" },
+                {
+                  validator: validateConfirmPassword,
+                },
+              ]}
+              width="md"
+            />
+            <div className="flex justify-between">
+              <ProFormSelect
+                name="gender"
+                label="Gender"
+                placeholder="Gender"
+                options={[
+                  { label: "Male", value: "MALE" },
+                  { label: "Female", value: "FEMALE" },
+                  { label: "Other", value: "OTHER" },
+                ]}
+                rules={[{ required: true, message: "Please select gender" }]}
+              />
+              <ProFormDatePicker
+                placeholder="Birthday"
+                name="dob"
+                label="Birthday"
+                rules={[{ required: true, message: "Please enter birthday" }]}
+              />
+            </div>
+          </ProForm>
+          <div className="sign-up-login mt-[20px]">
+            <p className="sign-up-login-desc">
+              Already have an account?{" "}
+              <Link to={ROUTES.login}>
+                <strong>Sign In</strong>
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
