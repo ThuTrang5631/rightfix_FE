e72ffdfb68c1c87b9d7d5656ad1731a713b29ac8
Trang Le <trang.le@mail.finviet.com.vn>
2024-07-08 22:29:41 +0700

    feat: UI header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,39 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import { MenuOutlined } from "@ant-design/icons";
+
 const Header = () => {
   return (
     <header className="header">
       <div className="header-logo">
-        <img alt="logo" />
+        <img alt="logo" src={logo} />
       </div>
       <ul className="header-menu">
-        <li className="header-item">Sản phẩm</li>
-        <li className="header-item">Công nghệ</li>
-        <li className="header-item">Dịch vụ</li>
-        <li className="header-item">Phụ tùng</li>
-        <li className="header-item">Khuyến mãi</li>
+        <li className="header-item">
+          <Link to="">Sản phẩm</Link>
+        </li>
+        <li className="header-item">
+          <Link to="">Công nghệ</Link>
+        </li>
+        <li className="header-item">
+          <Link to="">Dịch vụ</Link>
+        </li>
+        <li className="header-item">
+          <Link to="">Phụ tùng</Link>
+        </li>
+        <li className="header-item">
+          <Link to=""></Link>Khuyến mãi
+        </li>
+        <li className="header-item">
+          <Link to="">Đại lý</Link>
+        </li>
+        <li className="header-item">
+          <Link to="">New me Discover</Link>
+        </li>
       </ul>
+      <button className="header-btn-nav">
+        <MenuOutlined style={{ fontSize: "25px" }} />
+      </button>
     </header>
   );
 };
