9b410607b1289b92ce722bcd7ae782c77f8f9125
Trang Le <trang.le@mail.finviet.com.vn>
2024-07-09 17:03:29 +0700

    fix: header UI

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-item">
+        <h2 className="footer-contact-title">LIÊN HỆ</h2>
+        <p className="footer-contact-desc">
+          Hãy luôn giữ liên lạc với chúng tôi
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
