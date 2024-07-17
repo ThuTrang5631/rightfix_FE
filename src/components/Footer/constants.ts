277253bbd8d9adbd9ca9668a6e4fe3d18367b8ea
Trang Le <trang.le@mail.finviet.com.vn>
2024-07-17 16:38:31 +0700

    fix: footer

---
+++ src/components/Footer/constants.ts
@@ -0,0 +1,39 @@
+import iconPhone from "../../assets/icons/mail.svg";
+import iconMail from "../../assets/icons/phone-call.svg";
+import iconTwitter from "../../assets/icons/twitter.svg";
+import iconLinkedIn from "../../assets/icons/linkedin.svg";
+import iconGithub from "../../assets/icons/github.svg";
+import iconFacebook from "../../assets/icons/facebook.svg";
+import iconSocial from "../../assets/icons/icon-social.svg";
+import iconDribble from "../../assets/icons/dribble.svg";
+
+export const footerData: any = {
+  Support: [
+    "Cancellation Policy",
+    "Register Bike",
+    "Return Policy",
+    "Shipping Info",
+    "Support",
+    "Warranty",
+  ],
+  "Electric Bike": [
+    "All Electric Bike",
+    "Accessories",
+    "Gift Cards",
+    "Promotions",
+  ],
+  Company: ["About us", "Contact us", "Blog", "Videos"],
+  Contact: {
+    [iconPhone]: "+1 (907) 555-0101",
+    [iconMail]: "curtis.weaver@example.com",
+  },
+};
+
+export const listIconSocial = [
+  iconTwitter,
+  iconLinkedIn,
+  iconFacebook,
+  iconGithub,
+  iconSocial,
+  iconDribble,
+];
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,73 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/Logo.svg";
+import { ROUTES } from "../../utils/constants";
+import { footerData, listIconSocial } from "./constants";
+import { CopyrightOutlined } from "@ant-design/icons";
+
+interface IFooterSectionProps {
+  title: string;
+  items: any;
+}
+
+const FooterSection = ({ title, items }: IFooterSectionProps) => (
+  <div className="footer-item">
+    <h6 className="footer-item-title">{title}</h6>
+    <ul className="footer-wrap-link">
+      {Array.isArray(items)
+        ? items?.map((item, index) => (
+            <li className="footer-link" key={index}>
+              <Link to="">{item}</Link>
+            </li>
+          ))
+        : Object.entries(items).map(([src, desc], index) => (
+            <li className="footer-link flex items-center" key={index}>
+              <img className="footer-link-icon" src={src} alt="icon" />{" "}
+              <a
+                href={
+                  src.includes("phone-call.svg")
+                    ? `tel:${desc}`
+                    : `mailto: ${desc}`
+                }
+              >
+                {desc}
+              </a>
+            </li>
+          ))}
+    </ul>
+  </div>
+);
+
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-item">
-        <h2 className="footer-contact-title">LIÊN HỆ</h2>
-        <p className="footer-contact-desc">
-          Hãy luôn giữ liên lạc với chúng tôi
+      <div className="footer-top">
+        <div className="footer-item">
+          <Link to={ROUTES.home}>
+            <img alt="logo" src={logo} className="footer-logo" />
+          </Link>
+          <h2 className="footer-title">
+            Design amazing digital experiences that create more happy in the
+            world.
+          </h2>
+        </div>
+        {Object.entries(footerData)?.map(([title, item], index) => (
+          <FooterSection key={index} title={title} items={item} />
+        ))}
+      </div>
+      <div className="footer-bottom">
+        <p className="footer-copyright">
+          <CopyrightOutlined />
+          2077 ElectroBike. All rights reserved.
         </p>
+        <ul className="footer-social flex gap-[24px]">
+          {listIconSocial.map((icon, index) => (
+            <li className="footer-social-item" key={index}>
+              <Link to="" className="footer-social-link">
+                <img src={icon} />
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </footer>
   );
